20264f6531b2c45eee778b1861b844363ed73b6d
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-05-24 14:02:04 +0530

    Create 3.2.printReverseNaturalNum.cpp

---
+++ 3.2.printReverseNaturalNum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter number";
+    cin>>n;
+    while(n>0){
+        cout<<n<<" ";
+        n--;
+    }
+    return 0;
+}

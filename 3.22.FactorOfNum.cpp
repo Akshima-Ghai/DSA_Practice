67e03214799b0ddea4e2c5fce1142994f22d15db
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-05-25 21:30:00 +0530

    Create 3.22.FactorOfNum.cpp

---
+++ 3.22.FactorOfNum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+    cin>>num;
+    for(int i=1;i<=num;i++){
+        if(num%i==0){
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}

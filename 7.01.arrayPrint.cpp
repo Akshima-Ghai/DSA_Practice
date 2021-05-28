add9143372711a8519479b31463d0a0e27fd1b2c
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-05-28 15:14:40 +0530

    Create 7.01.arrayPrint.cpp

---
+++ 7.01.arrayPrint.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[10];
+    for(int i=0;i<10;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<10;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}

15379e2fd04ce206a307d4b1578e6adff32a4502
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-05-31 01:51:34 +0530

    Create 7.20.copyArray.cpp

---
+++ 7.20.copyArray.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,count=0;
+    cin>>n;
+    int arr[n],crr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        crr[i]=arr[i];
+        cout<<crr[i]<<" ";
+    }
+    
+    return 0;
+}

3de93a8fc91bcacb2401f3f2e42ee865f3d956ad
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-05-30 23:22:57 +0530

    Create 7.13.readInputArray.cpp

---
+++ 7.13.readInputArray.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+        cout<<arr[i]<<" ";
+        
+    }
+    
+
+    return 0;
+}

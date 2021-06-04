09c9d458b8da4649a01e15009bb3c258774993b8
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-06-04 22:28:22 +0530

    Create 9.02.concatString.cpp

---
+++ 9.02.concatString.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s1,s2,s3;
+    getline(cin,s1);
+    getline(cin,s2);
+    s3=s1+' ' +s2;
+    cout<<s3;
+
+    return 0;
+}

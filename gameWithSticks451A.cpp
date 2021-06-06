03cd8adc0000d057cdd3a02be27474ea43ea8ee2
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-06-06 14:44:52 +0530

    Create gameWithSticks451A.cpp

---
+++ gameWithSticks451A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int sum = n + m;
+    int name = 0;
+    while (sum > 1)
+    {
+        n--;
+        m--;
+        name++;
+        if (n == 0 or m == 0)
+        {
+            break;
+        }
+        sum = n + m;
+    }
+
+    if (name % 2 == 0)
+    {
+        cout << "Malvika";
+    }
+    else
+    {
+        cout << "Akshat";
+    }
+    return 0;
+}

5649b9be5cbf44df322f6b8e239fdf3e8d652012
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-06-07 16:17:31 +0530

    Create youngPhysicist.cpp

---
+++ youngPhysicist.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,x,y,z,sum1=0,sum2=0,sum3=0;
+    cin>>n;
+    int arr[n];
+    while(n--){
+        cin>>x>>y>>z;
+        sum1+=x;
+        sum2+=y;
+        sum3+=z;
+    }
+    if(sum1==0 && sum2==0 && sum3==0){
+        cout<<"YES"<<endl;
+    }else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}

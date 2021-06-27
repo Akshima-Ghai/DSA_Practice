8b300a46430a3e1138d41861978731fa86714be0
Akshima-Ghai <58785998+Akshima-Ghai@users.noreply.github.com>
2021-06-27 12:40:59 +0530

    Create reverseStringRecursion.cpp

---
+++ reverseStringRecursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void reverse(string s)
+{
+    if (s.length() == 0)
+    {
+        return;
+    }
+    string ros = s.substr(1);
+    reverse(ros);
+    cout << s[0];
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    reverse(s);
+    return 0;
+}

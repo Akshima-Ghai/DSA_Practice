d9d19655a1bff604f9b6dbc8a05d3862822419a0
Aman Kuliyal <1devyansh1@gmail.com>
2021-07-23 23:21:48 +0530

    Update 2.11.LeapYear.cpp

--- 2.11.LeapYear.cpp
+++ 2.11.LeapYear.cpp
@@ -6,19 +6,10 @@ int main()
 {
     int year;
     cin>>year;
-    if(year%4==0){
-        if(year%100==0){
-            if(year%400==0){
-                cout<<year<<" is a leap year";
-            }else{
-                cout<<year<<" is not a leap year";
-            }
-        }else{
-            cout<<year<<" is a leap year";
-        }
-    }else{
-        cout<<year<<" is not a leap year";
-    }
-
+    //(year%4==0) ? ((year%100==0) ? ((year%400==0) ? cout<<year<<" is a leap year" : cout<<year<<" is not a leap year") : cout<<year<<" is a leap year") : cout<<year<<" is not a leap year";
+    if((year%400==0)||(year%4==0 && year%100!=0))
+		cout<<year<<" is Leap year"<<endl;
+	else
+		cout<<year<<" is not Leap year"<<endl;
     return 0;
 }
